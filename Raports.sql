a3262f6f26f83f317cdef57fac953e93c5cec466
„Mateusz” <dsw60356@student.dsw.edu.pl>
2026-05-15 16:22:29 +0200

    Dane

---
+++ Raports.sql
@@ -0,0 +1,11 @@
+insert into notatnik_kcal (data_spoz, suma_kcal, suma_tlus, suma_wegl, suma_bial, suma_cukr)
+select 
+current_date,
+round(sum(wim.kcal * (dd.waga_zjedzona/100.0)), 1) as kalorie, 
+round(SUM(wim.tluszcze * (dd.waga_zjedzona/100.0)), 1) as tluszcze,
+round(SUM(wim.wegle * (dd.waga_zjedzona /100.0)), 1) as wegle,
+round(SUM(wim.bialko * (dd.waga_zjedzona/100.0)), 1) as bialko, 
+round(sum(wim.cukry * (dd.waga_zjedzona/100.0)), 1) as cukry
+from dziennik_dzienny dd 
+join warianty_i_makro wim on dd.id_wariantu = wim.id_wariantu
+where dd.data_spoz = current_date;
