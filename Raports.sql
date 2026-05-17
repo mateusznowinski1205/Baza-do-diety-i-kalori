ea50d18a3d59bdddc51d43d8febb7e88ed622e39
„Mateusz” <dsw60356@student.dsw.edu.pl>
2026-05-17 19:06:10 +0200

    Lekka aktulaizacja raportów

--- Raports.sql
+++ Raports.sql
@@ -9,3 +9,14 @@ round(sum(wim.cukry * (dd.waga_zjedzona/100.0)), 1) as cukry
 from dziennik_dzienny dd 
 join warianty_i_makro wim on dd.id_wariantu = wim.id_wariantu
 where dd.data_spoz = current_date;
+
+
+select 
+	(sum(kcal * (waga_zjedzona / 100.0))),
+	(sum(tluszcze * (waga_zjedzona / 100.0))),
+	(sum(wegle * (waga_zjedzona / 100.0))),
+	(sum(bialko * (waga_zjedzona / 100.0))),
+	(sum(cukry * (waga_zjedzona / 100.0)))
+from warianty_i_makro wim join
+dziennik_dzienny dd on wim.id_wariantu = dd.id_wariantu
+where data_spoz = current_date;
